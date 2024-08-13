f2ff1d2e863c0a8016e9fb6eb76813779c99da05
Vishal Machchha <95867828+vishalIII@users.noreply.github.com>
2024-08-13 20:05:34 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -11,7 +11,7 @@ const port = process.env.PORT || 3000;
 connectToMongo();
 
 const corsOptions = {
-  origin: 'http://localhost:5173', 
+  origin: 'https://66bb6ed394302c376344fae7--celebrated-tanuki-cc8e6d.netlify.app', 
   methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
   allowedHeaders: ['Authorization', 'Content-Type'],
   credentials: true,
